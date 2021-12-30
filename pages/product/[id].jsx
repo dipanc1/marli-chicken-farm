c4d3103d48d74baad158254b8c490e87fa78dc4f
dipancnextgen <dipanc@nextgeninnovation.com>
2021-12-30 11:08:44 +0530

    second day, on order tracking page now

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,13 +1,17 @@
 import styles from "../../styles/Product.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
 
 function Product() {
+
+    const [size, setSize] = useState(0);
+
     const chicken = {
         id: 1,
         name: "Chicken",
-        price: "1.99",
-        description: "A delicious chicken",
+        price: [2.99, 13.99, 24.99],
+        desc: "A delicious chicken",
         image: "/img/pizza.png",
         imageAlt: "Chicken"
     }
@@ -21,9 +25,52 @@ function Product() {
                 </div>
             </div>
             <div className={styles.right}>
-
+                <h1 className={styles.title}>{chicken.name}</h1>
+                <span className={styles.price}>${chicken.price[size]}</span>
+                <p className={styles.desc}>{chicken.desc}</p>
+                <h3 className={styles.choose}>Choose the size</h3>
+                <div className={styles.sizes}>
+                    <div className={styles.size} onClick={() => setSize(0)}>
+                        <Image alt="" src="/img/size.png" layout="fill" />
+                        <span className={styles.number}>Small</span>
+                    </div>
+                    <div className={styles.size} onClick={() => setSize(1)}>
+                        <Image alt="" src="/img/size.png" layout="fill" />
+                        <span className={styles.number}>Medium</span>
+                    </div>
+                    <div className={styles.size} onClick={() => setSize(2)}>
+                        <Image alt="" src="/img/size.png" layout="fill" />
+                        <span className={styles.number}>Large</span>
+                    </div>
+                </div>
+                <h3 className={styles.choose}>
+                    Choose Chicken Type
+                </h3>
+                <div className={styles.ingredients}>
+                    <div className={styles.option}>
+                        <input
+                            type="radio" id="laying" name="checkbox"
+                            className={styles.checkbox} value="1" onclick="selectOnlyThis(this)"/>
+                        <label htmlFor="laying">Laying</label>
+                    </div>
+                    <div className={styles.option}>
+                        <input
+                            type="radio" id="meat-producing" name="checkbox"
+                            className={styles.checkbox} value="2" onclick="selectOnlyThis(this)"/>
+                        <label htmlFor="meat-producing">Meat-Producing</label>
+                    </div>
+                    <div className={styles.option}>
+                        <input
+                            type="radio" id="dual-purpose breed" name="checkbox"
+                            className={styles.checkbox} value="3" onclick="selectOnlyThis(this)" />
+                        <label htmlFor="dual-purpose breed">Dual-Purpose Breed</label>
+                    </div>
+                </div>
+                <div className={styles.add}>
+                    <input type="number" defaultValue={1} className={styles.quantity} />
+                    <button className={styles.button}>Add to Cart</button>
+                </div>
             </div>
-
         </div>
     )
 }
