b36911fcf6ed336a4c8e1265ec8de75e7f0584ba
dipancnextgen <dipanc@nextgeninnovation.com>
2022-01-10 14:58:22 +0530

    Admin Page Added

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -1,8 +1,27 @@
+import Order from "../../../models/Order";
+import dbConnect from "../../../util/mongo";
+
 const handler = async(req, res) => {
     const { method, query: { id } } = req;
 
-    if (method === 'GET') {}
-    if (method === 'PUT') {}
+    await dbConnect();
+
+    if (method === 'GET') {
+        try {
+            const order = await Order.findById(id);
+            res.status(200).json(order);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+    if (method === 'PUT') {
+        try {
+            const order = await Order.findByIdAndUpdate(id, req.body, { new: true });
+            res.status(201).json(order);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
     if (method === 'DELETE') {}
 }
 
--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
 
     if (method === 'PUT') {
         try {
-            const product = await Product.create(req.body);
+            const product = await Product.findByIdAndUpdate(id, req.body, { new: true });
             res.status(201).json(product);
         } catch (error) {
             res.status(500).json(error);
@@ -26,8 +26,8 @@ export default async function handler(req, res) {
 
     if (method === 'DELETE') {
         try {
-            const product = await Product.create(req.body);
-            res.status(201).json(product);
+            const product = await Product.findByIdAndDelete(id);
+            res.status(200).json("The product is Deleted!");
         } catch (error) {
             res.status(500).json(error);
         }
