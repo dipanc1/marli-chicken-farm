c05c91a3a7b116a815e07ea6a5aba44c224c5670
dipanc1 <dipanchhabra@gmail.com>
2022-01-10 15:46:16 +0530

    login API Using Cookie

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,14 +1,19 @@
+import cookie from "cookie";
+
 const handler = (req, res) => {
-    const { username, password } = req.body;
-    const user = {
-        username,
-        password,
-    };
-    const token = jwt.sign(user, process.env.SECRET_KEY);
-    res.json({
-        success: true,
-        token,
-    });
+    if (req.method === "POST") {
+        const { username, password } = req.body;
+        if (username === process.env.USERNAME && password === process.env.PASSWORD) {
+            res.setHeader("Set-Cookie", cookie.serialize("token", process.env.TOKEN, {
+                maxAge: 60 * 60,
+                sameSite: 'strict',
+                path: '/',
+            }));
+            res.status(200).json('Success');
+        } else {
+            res.status(400).json('Wrong Credentials');
+        }
+    }
 }
 
 export default handler;
