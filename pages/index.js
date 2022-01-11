b648ca2d30c4a15e79be78bccac7d0545befaf6a
dipanc1 <dipanchhabra@gmail.com>
2022-01-11 11:31:40 +0530

    add animal fucntionality

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 import Head from 'next/head'
+import { useState } from 'react'
+import Add from '../components/Add'
+import AddButton from '../components/AddButton'
 import Featured from '../components/Featured'
 import ProductList from '../components/ProductList'
 
 
-export default function Home({ products }) {
+export default function Home({ products, admin }) {
+  const [close, setClose] = useState(true)
 
   return (
     <div>
@@ -14,16 +18,27 @@ export default function Home({ products }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
+      {admin && <AddButton setClose={setClose} />}
       <ProductList products={products} />
+      {!close && <Add setClose={setClose} />}
     </div>
   )
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (ctx) => {
+
+  const myCookie = ctx.req?.cookies || '';
+  let admin = false;
+
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true;
+  }
+
   const res = await axios.get("http://localhost:3000/api/products")
   return {
     props: {
-      products: res.data
+      products: res.data,
+      admin
     }
   }
 };
